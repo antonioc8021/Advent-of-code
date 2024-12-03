37e7940782f002c648c13ea67a661f7b95c99c43
antonioc8021 <antonioc8021@gmail.com>
2024-12-03 13:39:15 +0100

    day 1 completed

--- day1/star2.py
+++ day1/star2.py
@@ -1,1 +1,17 @@
 from list import Lista
+
+
+class numeros_repetidos(Lista):
+    # inicializo un contador de veces que se repiten los números
+    # inicializo un sumatorio de todos los números que se repiten
+    contador_numeros_repetidos = 0
+    sumatorio = 0
+    for clave in Lista.clave:
+        for valor in Lista.valor:
+            if clave == valor:
+                contador_numeros_repetidos += 1
+        sumatorio += clave * contador_numeros_repetidos
+        print(contador_numeros_repetidos)
+        # restablezco el valor a 0 para que no se sume el contador al valor anterior
+        contador_numeros_repetidos = 0
+    print(sumatorio)
